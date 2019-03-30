f61f0eff047a56a20d33593f3a09403c15c0e8e7
Nicholas Jackson <ngjackson16@transy.edu>
2019-03-29 22:14:38 -0400

    Added comments

--- src/main/java/com/ngjackson/controllers/HelloWorld.java
+++ src/main/java/com/ngjackson/controllers/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.ngjackson.controllers;
 
+import com.ngjackson.model.Query;
+import com.ngjackson.util.QueryUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class HelloWorld {
@@ -9,6 +14,13 @@ public class HelloWorld {
   @RequestMapping("/")
   public String getIndex() {
     return "Hello world!";
+  }
+
+  @RequestMapping("/getQueries")
+  public List<Query> getQueries() throws IOException {
+    QueryUtil queryUtil = new QueryUtil();
+
+    return queryUtil.getQueries();
   }
 
 }
--- src/main/java/com/ngjackson/util/QueryUtil.java
+++ src/main/java/com/ngjackson/util/QueryUtil.java
@@ -1,6 +1,7 @@
 package com.ngjackson.util;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.ngjackson.model.Query;
@@ -11,28 +12,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * A util with various methods regarding queries.
+ */
 public class QueryUtil {
 
   @Value("${queries.json.filePath}")
   private String queriesJsonFilePath;
 
-  public List<Query> getQueries() throws URISyntaxException, IOException {
+  /**
+   * Gets the Query object from the queries JSON file.
+   *
+   * @return A list of Query objects
+   * @throws IOException if we're unable to read the file
+   */
+  public List<Query> getQueries() throws IOException {
+
+    Gson gson = new Gson();
 
     String queriesJson = getQueriesJson();
 
+    // Create a parser and parse the JSON object
     JsonParser parser = new JsonParser();
     JsonObject queriesJsonObject = parser.parse(queriesJson).getAsJsonObject();
 
+    // Query down one level so we don't need a "queryTemplates" object
+    JsonArray queryTemplates = queriesJsonObject.get("queryTemplates").getAsJsonArray();
 
-    return Arrays.asList(gson.fromJson(queriesJson, Query[].class));
+    // Convert that JSON array to the POJOs
+    return Arrays.asList(gson.fromJson(queryTemplates, Query[].class));
   }
 
-  private String getQueriesJson() throws IOException, URISyntaxException {
+  /**
+   * Gets the JSON for the queries.
+   *
+   * @return A string with the JSON object
+   * @throws IOException if we're unable to read the file
+   */
+  private String getQueriesJson() throws IOException {
     ClassPathResource resource = new ClassPathResource(this.queriesJsonFilePath);
     InputStream inputStream = resource.getInputStream();
 
